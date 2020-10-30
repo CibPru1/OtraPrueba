ccc14c4c84390e810afa9f903778cd1997c96390
Yorladis <i201814760@cibertec.edu.pe>
2020-10-29 20:33:10 -0500

    Se realizo un cambio

--- ProyectoPrueba/src/com/obispo/Prueba01.java
+++ ProyectoPrueba/src/com/obispo/Prueba01.java
@@ -5,7 +5,7 @@ public class Prueba01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Esto es una prueba");
-
+        System.out.println("Prueba conforme");
 	}
 
 }
